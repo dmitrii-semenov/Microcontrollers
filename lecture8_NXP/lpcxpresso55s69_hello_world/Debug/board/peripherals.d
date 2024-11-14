584b63510d9f7e99cf8dc5ae06e3a2c7f88dc3a5
Dmitrii Semenov <240689@vutbr.cz>
2024-11-14 12:37:49 +0100

    Added lecture8

---
+++ lecture8_NXP/lpcxpresso55s69_hello_world/Debug/board/peripherals.d
@@ -0,0 +1,35 @@
+board/peripherals.o board/peripherals.d: ../board/peripherals.c \
+ ../board/peripherals.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_common.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\device/fsl_device_registers.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\device/LPC55S69_cm33_core0.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\CMSIS/core_cm33.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\CMSIS/cmsis_version.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\CMSIS/cmsis_compiler.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\CMSIS/cmsis_gcc.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\CMSIS/mpu_armv8.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\device/system_LPC55S69_cm33_core0.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\device/LPC55S69_cm33_core0_features.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_common_arm.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_clock.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_common.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_reset.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_ctimer.h \
+ C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_clock.h
+../board/peripherals.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_common.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\device/fsl_device_registers.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\device/LPC55S69_cm33_core0.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\CMSIS/core_cm33.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\CMSIS/cmsis_version.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\CMSIS/cmsis_compiler.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\CMSIS/cmsis_gcc.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\CMSIS/mpu_armv8.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\device/system_LPC55S69_cm33_core0.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\device/LPC55S69_cm33_core0_features.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_common_arm.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_clock.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_common.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_reset.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_ctimer.h:
+C:\Users\240689\git\Microcontrollers\lpcxpresso55s69_hello_world\drivers/fsl_clock.h:
